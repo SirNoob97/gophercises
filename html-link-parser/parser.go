2083b4fe68290a9942eb60178295293731c99f40
Martin <martinald467@gmail.com>
2021-01-17 17:39:01 +0100

    format

--- html-link-parser/parser.go
+++ html-link-parser/parser.go
@@ -22,12 +22,12 @@ func Parser(r io.Reader) ([]Link, error) {
 	}
 
 	nodes := linkNodes(doc)
-  var links []Link
+	var links []Link
 	for _, node := range nodes {
-    links = append(links, buildLinks(node))
+		links = append(links, buildLinks(node))
 	}
 
-  return links, nil
+	return links, nil
 }
 
 func linkNodes(n *html.Node) []*html.Node {
@@ -47,7 +47,7 @@ func buildLinks(n *html.Node) Link {
 	var ret Link
 	for _, attr := range n.Attr {
 		if attr.Key == "href" {
-      ret.Href = "Href:" + attr.Val
+			ret.Href = "Href:" + attr.Val
 			break
 		}
 	}
@@ -56,12 +56,12 @@ func buildLinks(n *html.Node) Link {
 }
 
 func text(n *html.Node) string {
-  if n.Type == html.TextNode {
-    return n.Data
-  }
-  if n.Type != html.ElementNode {
-    return ""
-  }
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+	if n.Type != html.ElementNode {
+		return ""
+	}
 
 	var ret string
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
