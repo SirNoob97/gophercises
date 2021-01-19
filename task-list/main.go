4cb86c135b061ed5dfa5bd1378acebda474338dd
Martin <martinald467@gmail.com>
2021-01-19 17:14:46 +0100

    add the route to the boltdb file

--- task-list/main.go
+++ task-list/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "github.com/SirNoob97/gophercises/task-list/cmd"
+import (
+	"log"
+	"path/filepath"
+
+	"github.com/SirNoob97/gophercises/task-list/cmd"
+	"github.com/SirNoob97/gophercises/task-list/db"
+	homedir "github.com/mitchellh/go-homedir"
+)
 
 func main() {
+	home, err := homedir.Dir()
+	if err != nil {
+		exit(err.Error())
+	}
+
+	dbPath := filepath.Join(home, "tasks.db")
+	err = db.Init(dbPath)
+	if err != nil {
+		exit(err.Error())
+	}
+
 	cmd.Execute()
+}
+
+func exit(msg string) {
+	log.Fatal(msg)
 }
