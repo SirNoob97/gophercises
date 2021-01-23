2989c3802041ff01fdc57b568b55d95e4bef34a4
Martin <martinald467@gmail.com>
2021-01-23 19:29:59 +0100

    complete file-renaming

--- file-renaming/main.go
+++ file-renaming/main.go
@@ -2,30 +2,54 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"strconv"
-	"strings"
+	"os"
+	"path/filepath"
+	"regexp"
 )
 
-func main() {
-	fileName := "birthday_001.txt"
-	newName, err := match(fileName, 4)
-	if err != nil {
-		log.Fatal("Not Match")
-	}
-	fmt.Println(newName)
+type file struct {
+	name string
+	path string
 }
 
-func match(fileName string, total int) (string, error) {
-	pieces := strings.Split(fileName, ".")
-	ext := pieces[len(pieces)-1]
-	tmp := strings.Join(pieces[0:len(pieces)-1], ".")
-	pieces = strings.Split(tmp, "_")
-	name := strings.Join(pieces[0:len(pieces)-1], "_")
-	number, err := strconv.Atoi(pieces[len(pieces)-1])
-	if err != nil {
+type matchResult struct {
+	base  string
+	index int
+	ext   string
+}
+
+var pattern = regexp.MustCompile("^(.+?) ([0-9]{4}) [(]([0-9]+) of ([0-9]+)[)][.](.+?)$")
+var replaceString = "$2 - $1 - $3 of $4.$5"
+
+func main() {
+	walkDir := "sample"
+	var toRename []string
+	filepath.Walk(walkDir, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		if _, err := match(info.Name()); err == nil {
+			toRename = append(toRename, path)
+		}
+		return nil
+	})
+
+	for _, oldPath := range toRename {
+		dir := filepath.Dir(oldPath)
+		filename := filepath.Base(oldPath)
+		newFileName, _ := match(filename)
+		newPath := filepath.Join(dir, newFileName)
+		fmt.Printf("mv %s => %s\n", oldPath, newPath)
+		err := os.Rename(oldPath, newPath)
+		if err != nil {
+			fmt.Printf("Error renaming: %s to %s. %s\n", oldPath, newPath, err.Error())
+		}
+	}
+}
+
+func match(fileName string) (string, error) {
+	if !pattern.MatchString(fileName) {
 		return "", fmt.Errorf("%s didn't match the pattern", fileName)
 	}
-
-	return fmt.Sprintf("%s - %d of %d.%s", strings.Title(name), number, total, ext), nil
+	return pattern.ReplaceAllString(fileName, replaceString), nil
 }
